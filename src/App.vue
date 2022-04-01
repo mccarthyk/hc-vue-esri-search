1f5ac0ad7064e5394d6bdbb2b4a839d4495434cc
Kevin McCarthy <mccarthyk@hcflgov.net>
2022-04-01 17:47:18 -0400

    init lib

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,154 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import {
+  // search component
+  SearchBootstrap,
+
+  // composition api
+  featureLayerProps,
+  searchResults,
+  queryFeatures,
+
+  // optional
+  input,
+  searchProps,
+  suggestions,
+  features,
+} from './lib'
+import { watch } from 'vue'
+
+// set feature layer url to query against
+featureLayerProps.url =
+  'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Counties/FeatureServer/0'
+
+// watch for search results
+watch(
+  () => searchResults.data,
+  async () => {
+    try {
+      const [firstResult] = searchResults.data
+
+      // query result's extent agains feature layer
+      const queriedFeatures = await queryFeatures(firstResult?.extent, {
+        // returnGeometry: true,
+      })
+
+      // fun with features!
+      console.log('features', queriedFeatures)
+    } catch (error) {
+      console.warn(error)
+    }
+  }
+)
+
+// set input value, useful for dev
+input.value = '601 E Kennedy Blvd, Tampa'
+// searchProps.activeSourceIndex = 1
+
+watch(input, () => {
+  // searchResults.data = []
+  features.data = null
+})
+
+// options
+import { reactive } from 'vue'
+const options = reactive({
+  size: 1,
+  sources: 1,
+})
 </script>
 
+<!--  -->
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <SearchBootstrap
+    :small="options.size === 0"
+    :large="options.size === 2"
+    :hc-sources="options.sources === 1"
+  />
+
+  <section class="bg-secondary p-3 mt-5">
+    <div class="d-grid gap-2 mb-3">
+      <a href="./docs/" target="_blank" class="btn btn-sm btn-light">
+        Documentation
+      </a>
+    </div>
+
+    <details open class="card">
+      <summary class="card-header">Options</summary>
+
+      <div class="card-body">
+        <label class="form-label w-100">Use HC Search Sources</label>
+        <div
+          v-for="(bool, i) in ['false', 'true']"
+          class="form-check form-check-inline"
+        >
+          <input
+            class="form-check-input"
+            type="radio"
+            name="inlineRadioOptions"
+            :id="`hc-sources-${i}`"
+            :value="i"
+            v-model="options.sources"
+          />
+          <label class="form-check-label" :for="`hc-sources-${i}`">
+            {{ bool }}
+          </label>
+        </div>
+      </div>
+
+      <div class="card-body">
+        <label for="featureLayerUrl" class="form-label">
+          Feature Layer URL to query
+        </label>
+        <input
+          type="email"
+          class="form-control"
+          id="featureLayerUrl"
+          placeholder="https://..."
+          v-model="featureLayerProps.url"
+        />
+      </div>
+
+      <div class="card-body">
+        <label class="form-label w-100">Size</label>
+        <div
+          v-for="(size, i) in ['small', 'medium', 'large']"
+          class="form-check form-check-inline"
+        >
+          <input
+            class="form-check-input"
+            type="radio"
+            name="size"
+            :id="`size-${size}`"
+            :value="i"
+            v-model="options.size"
+          />
+          <label class="form-check-label" :for="`size-${size}`">{{
+            size
+          }}</label>
+        </div>
+      </div>
+    </details>
+
+    <details open class="card">
+      <summary class="card-header">Suggestions</summary>
+      <pre class="card-body">{{ suggestions }}</pre>
+    </details>
+
+    <details open class="card">
+      <summary class="card-header">Search Results</summary>
+      <pre class="card-body">{{ searchResults }}</pre>
+    </details>
+
+    <details open class="card">
+      <summary class="card-header">Queried Features</summary>
+      <pre class="card-body">{{ features }}</pre>
+    </details>
+  </section>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+details:not(:last-child) {
+  margin-bottom: 1rem;
 }
 </style>
---
+++ src/components/Search/Bootstrap.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+import { watch } from 'vue'
+import {
+  input,
+  suggest,
+  suggestions,
+  searchProps,
+  activeSource,
+  search,
+  searchResults,
+} from '../../lib'
+import { locator, folio } from '../../lib/search-sources'
+
+const props =
+  defineProps<{ small?: boolean; large?: boolean; hcSources?: boolean }>()
+
+watch(
+  () => props.hcSources,
+  () => {
+    searchProps.includeDefaultSources = props.hcSources ? false : true
+    searchProps.sources = props.hcSources ? [locator, folio] : undefined
+  },
+  { immediate: true }
+)
+</script>
+
+<template>
+  <form @submit.prevent="search">
+    <fieldset v-if="searchProps.sources?.length">
+      <legend>Search Source:</legend>
+      <div
+        v-for="(source, i) in searchProps.sources"
+        class="form-check form-check-inline"
+      >
+        <input
+          type="radio"
+          name="search-source"
+          :value="i"
+          :id="`source-${i}`"
+          v-model="searchProps.activeSourceIndex"
+          class="form-check-input"
+        />
+        <label :for="`source-${i}`" class="form-check-label">
+          {{ source.name }}
+        </label>
+      </div>
+    </fieldset>
+
+    <fieldset>
+      <legend>Search:</legend>
+
+      <div
+        class="input-group"
+        :class="{ 'input-group-sm': small, 'input-group-lg': large }"
+      >
+        <input
+          type="text"
+          class="form-control"
+          :placeholder="activeSource?.placeholder || `Find Address`"
+          :aria-label="activeSource?.placeholder || `Find Address`"
+          aria-describedby="search-button"
+          v-model="input"
+          @input="suggest"
+          list="suggestions"
+          required
+        />
+
+        <button
+          class="btn btn-outline-secondary"
+          type="submit"
+          id="search-button"
+        >
+          <div
+            v-if="searchResults.loading"
+            class="spinner-border spinner-border-sm"
+            role="status"
+          >
+            <span class="visually-hidden">Loading...</span>
+          </div>
+          <span v-else>Search</span>
+        </button>
+
+        <datalist id="suggestions">
+          <option v-for="{ text } in suggestions.data">
+            {{ text }}
+          </option>
+        </datalist>
+      </div>
+
+      <!-- status -->
+      <div class="form-text">
+        {{ searchResults.status }}
+      </div>
+    </fieldset>
+  </form>
+</template>
